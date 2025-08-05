a2959e935188bf2551df64078a52a7ae47f6bac2
Francesco Ciulla <francescociullaroma@gmail.com>
2025-08-05 07:49:20 +0000

    Generated by Spark: Fix all reported errors.

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import { ErrorBoundary } from "react-error-boundary";
 import "@github/spark/spark"
 
 import App from './App.tsx'
-import { ErrorFallback } from './ErrorFallback.tsx'
-import { ClerkAuthProvider } from './components/ClerkAuthProvider.tsx'
 
 import "./main.css"
 import "./index.css"
 
 createRoot(document.getElementById('root')!).render(
-  <ErrorBoundary FallbackComponent={ErrorFallback}>
-    <ClerkAuthProvider>
-      <App />
-    </ClerkAuthProvider>
-   </ErrorBoundary>
+  <App />
 )
